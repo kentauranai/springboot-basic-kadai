577e4a648bbd98c095a9bc6924a63c367c567298
kentauranai <kentapurovigent@gmail.com>
2026-01-30 14:15:27 +0900

    コミット

---
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/controller/ContactFormController.java
@@ -0,0 +1,36 @@
+package com.example.springkadaiform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.springkadaiform.form.ContactForm;
+
+import jakarta.validation.Valid;
+
+@Controller
+public class ContactFormController {
+
+    @GetMapping("/form")
+    public String showForm(Model model) {
+        model.addAttribute("contactForm", new ContactForm());
+        return "contactFormView";
+    }
+
+    @PostMapping("/confirm")
+    public String confirm(
+            @Valid @ModelAttribute("contactForm") ContactForm contactForm,
+            BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "contactFormView";
+        }
+
+        return "confirmView";
+    }
+}
+
